dd77571db996476ebd83c9c3d063191db36b14f3
daisuke-c <09p061@gmail.com>
2021-03-20 00:17:36 +0900

    本番環境での稼働を確認

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <nav class="header__nav">
+      <ul class="header__list">
+        <li class="header__item"><router-link to="/">HOME</router-link></li>
+        <li class="header__item"><a href="">エージェント検索</a></li>
+      </ul>
+      <ul class="header__list">
+        <li class="header__item"><a href="">ログイン</a></li>
+        <li class="header__item"><a href="">会員登録</a></li>
+        <li class="header__item"><router-link to="/signup">新規登録</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./components/css/header.css"></style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="signup">
+    <Header></Header>
+
+      <h3>新規会員登録</h3>
+    <table class="table">
+      <tr class="tr">
+        <td>
+          <label>ユーザ名</label><br/>
+          <input type="text" placeholder="userName" v-model="displayName" />
+        </td>
+      </tr>
+      <tr class="tr">
+        <td>
+          <label>メールアドレス</label><br/>
+          <input type="text" placeholder="E-mail" v-model="mailAdress" />
+        </td>
+      </tr>
+      <tr class="tr">
+        <td>
+          <label>パスワード(英数字8文字以上)</label><br/>
+          <input type="password" placeholder="Password" v-model="password" />
+        </td>
+      </tr>
+      <tr class="tr">
+        <td>
+          <label>パスワード</label><br/>
+          <input
+            type="password"
+            placeholder="PasswordCheck"
+            v-model="passwordCheck"
+          />
+        </td>
+      </tr>
+      <button class="btn" v-on:click="signUp">
+        <router-link to="/" class="routerBtn">会員登録</router-link></button
+      ><br />
+      <router-link to="/login" class="routerBtn"
+        >ログインはこちらから</router-link
+      >
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      displayName: '',
+      mailAdress: '',
+      password: '',
+      passwordCheck: '',
+    };
+  },
+};
+</script>
+
+<style scoped src="./components/css/signup.css">
+
+</style>
